b42b9563f7cb397bf928c9fe0ae2b9f6bfb08640
Spectra-2235 <smancera01@alumnos.uaq.mx>
2024-05-14 21:04:23 -0600

    Avance proyecto

---
+++ Lib/main.py
@@ -0,0 +1,170 @@
+# Importar la biblioteca CSV
+import csv
+
+# Declarar listas y variables
+materia=[]
+primerDia=[]
+sdoDia=[]
+inicioClase=[]
+finClase=[]
+entedendimientoClase_primerDia=[]
+entendimientoclases_sdoDia=[]
+
+listaDeValores=[]
+datosTotales=48
+datos_excelente= 17
+datos_Poco=16
+datos_Normal=15
+
+# Definir función para leer datos desde el archivo CSV
+def leer_datos():
+    global entendimientoclases_sdoDia, entendimientoclases_sdoDia
+    with open('Dataset.csv') as file:
+        csv_reader = csv.reader(file, delimiter=',')
+        next(csv_reader)  # Saltar la primera fila (encabezados)
+
+        for row in csv_reader:
+            # Leer datos y almacenar en listas respectivas
+            materia.append(row[0])
+            primerDia.append(row[1])
+            sdoDia.append(row[2])
+            inicioClase.append(row[3])
+            finClase.append(row[4])
+            entedendimientoClase_primerDia.append(row[5])
+            entendimientoclases_sdoDia.append(row[6])
+
+# Definir función para calcular estadísticas
+def sacar_stats():
+    # Inicializar contadores para diferentes variables
+    n = 0
+    n1, n2, n3, n4, n5, n6, n7, n8,n9,n10,n11,n12,n13,n14,n15,n16,n17,n18,n19,n20,n21,n22,n23,n24,n25,n26,n27,n28,n29,n30,n31,n32,n33,n34,n35,n36 = 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+    # Estadísticas para la variable 'dia'
+    for i in materia:
+        if i == 'Inteligencia de Negocios' and entedendimientoClase_primerDia[n] == 'Excelente':
+            n1 += 1
+        if i == 'Sistemas Inmersos y de realidad aumentada' and entedendimientoClase_primerDia[n] == 'Excelente':
+            n2 += 1
+        if i == 'Topico I' and entedendimientoClase_primerDia[n] == 'Excelente':
+            n3 += 1
+        if i == 'Administración de proyectos de software' and entedendimientoClase_primerDia[n] == 'Excelente':
+            n4 += 1
+        if i == 'Pruebas y aseguramiento de la calidad de software' and entedendimientoClase_primerDia[n] == 'Excelente':
+            n5 += 1
+        if i == 'Ingles VI' and entedendimientoClase_primerDia[n] == 'Excelente':
+            n6 += 1
+        if i == 'Inteligencia de Negocios' and entedendimientoClase_primerDia[n] == 'Normal':
+            n7 += 1
+        if i == 'Sistemas Inmersos y de realidad aumentada' and entedendimientoClase_primerDia[n] == 'Normal':
+            n8 += 1
+        if i == 'Topico I' and entedendimientoClase_primerDia[n] == 'Normal':
+            n9 += 1
+        if i == 'Administración de proyectos de software' and entedendimientoClase_primerDia[n] == 'Normal':
+            n10 += 1
+        if i == 'Pruebas y aseguramiento de la calidad de software' and entedendimientoClase_primerDia[n] == 'Normal':
+            n11 += 1
+        if i == 'Ingles VI' and entedendimientoClase_primerDia[n] == 'Normal':
+            n12 += 1
+        if i == 'Inteligencia de Negocios' and entedendimientoClase_primerDia[n] == 'Poco':
+            n13 += 1
+        if i == 'Sistemas Inmersos y de realidad aumentada' and entedendimientoClase_primerDia[n] == 'Poco':
+            n14 += 1
+        if i == 'Topico I' and entedendimientoClase_primerDia[n] == 'Poco':
+            n15 += 1
+        if i == 'Administración de proyectos de software' and entedendimientoClase_primerDia[n] == 'Poco':
+            n16 += 1
+        if i == 'Pruebas y aseguramiento de la calidad de software' and entedendimientoClase_primerDia[n] == 'Poco':
+            n17 += 1
+        if i == 'Ingles VI' and entedendimientoClase_primerDia[n] == 'Poco':
+            n18 += 1
+
+        if i == 'Inteligencia de Negocios' and entendimientoclases_sdoDia[n] == 'Excelente':
+            n19 += 1
+        if i == 'Sistemas Inmersos y de realidad aumentada' and entendimientoclases_sdoDia[n] == 'Excelente':
+            n20 += 1
+        if i == 'Topico I' and entendimientoclases_sdoDia[n] == 'Excelente':
+            n21 += 1
+        if i == 'Administración de proyectos de software' and entendimientoclases_sdoDia[n] == 'Excelente':
+            n22 += 1
+        if i == 'Pruebas y aseguramiento de la calidad de software' and entendimientoclases_sdoDia[n] == 'Excelente':
+            n23 += 1
+        if i == 'Ingles VI' and entendimientoclases_sdoDia[n] == 'Excelente':
+            n24 += 1
+        if i == 'Inteligencia de Negocios' and entendimientoclases_sdoDia[n] == 'Normal':
+            n25 += 1
+        if i == 'Sistemas Inmersos y de realidad aumentada' and entendimientoclases_sdoDia[n] == 'Normal':
+            n26 += 1
+        if i == 'Topico I' and entendimientoclases_sdoDia[n] == 'Normal':
+            n27 += 1
+        if i == 'Administración de proyectos de software' and entendimientoclases_sdoDia[n] == 'Normal':
+            n28 += 1
+        if i == 'Pruebas y aseguramiento de la calidad de software' and entendimientoclases_sdoDia[n] == 'Normal':
+            n29 += 1
+        if i == 'Ingles VI' and entendimientoclases_sdoDia[n] == 'Normal':
+            n30 += 1
+        if i == 'Inteligencia de Negocios' and entendimientoclases_sdoDia[n] == 'Poco':
+            n31 += 1
+        if i == 'Sistemas Inmersos y de realidad aumentada' and entendimientoclases_sdoDia[n] == 'Poco':
+            n32 += 1
+        if i == 'Topico I' and entendimientoclases_sdoDia[n] == 'Poco':
+            n33 += 1
+        if i == 'Administración de proyectos de software' and entendimientoclases_sdoDia[n] == 'Poco':
+            n34 += 1
+        if i == 'Pruebas y aseguramiento de la calidad de software' and entendimientoclases_sdoDia[n] == 'Poco':
+            n35 += 1
+        if i == 'Ingles VI' and entendimientoclases_sdoDia[n] == 'Poco':
+            n36 += 1
+        n += 1
+    # Almacenar resultados en listaDeValores
+    listaDeValores.extend([n1, n2, n3, n4, n5, n6, n7, n8,n9,n10,n11,n12,n13,n14,n15,n16,n17,n18,n19,n20,n21,n22,n23,n24,n25,n26,n27,n28,n29,n30,n31,n32,n33,n34,n35,n36])
+
+    # Estadísticas para la variable 'inicioClase'
+    n = 0
+    n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    for i in inicioClase:
+        if (i == '02:30' or i == '02:35' or i == '02:40') and entedendimientoClase_primerDia[n] == 'si':
+            n1 += 1
+
+        if (i == '02:45' or i == '02:50' or i == '02:55') and entedendimientoClase_primerDia[n] == 'si':
+            n2 += 1
+
+        if (i == '03:00' or i == '04:30' or i == '04:35') and entedendimientoClase_primerDia[n] == 'si':
+            n3 += 1
+
+        if (i == '04:40' or i == '04:45' or i == '04:50') and entedendimientoClase_primerDia[n] == 'si':
+            n4 += 1
+
+        if (i == '04:55' or i == '05:00' or i == '06:30') and entedendimientoClase_primerDia[n] == 'si':
+            n5 += 1
+
+        if (i == '06:35' or i == '06:40' or i == '06:45') and entedendimientoClase_primerDia[n] == 'si':
+            n6 += 1
+
+        if (i == '06:50' or i == '06:55' or i == '07:00') and entedendimientoClase_primerDia[n] == 'si':
+            n7 += 1
+
+        if (i == '02:30' or i == '02:35' or i == '02:40') and entedendimientoClase_primerDia[n] == 'no':
+            n8 += 1
+
+        if (i == '02:45' or i == '02:50' or i == '02:55') and entedendimientoClase_primerDia[n] == 'no':
+            n9 += 1
+
+        if (i == '03:00' or i == '04:30' or i == '04:35') and entedendimientoClase_primerDia[n] == 'no':
+            n10 += 1
+
+        if (i == '04:40' or i == '04:45' or i == '04:50') and entedendimientoClase_primerDia[n] == 'no':
+            n11 += 1
+
+        if (i == '04:55' or i == '05:00' or i == '06:30') and entedendimientoClase_primerDia[n] == 'no':
+            n12 += 1
+
+        if (i == '06:35' or i == '06:40' or i == '06:45') and entedendimientoClase_primerDia[n] == 'no':
+            n13 += 1
+
+        if (i == '06:50' or i == '06:55' or i == '07:00') and entedendimientoClase_primerDia[n] == 'no':
+            n14 += 1
+
+        n += 1
+    listaDeValores.extend([n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14])
+
+
